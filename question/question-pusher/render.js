3e96b50a2b4b1ae57b22b2f4621ca8332ed1fc65
vec <vec@moebaka.com>
2016-11-14 14:26:05 +0800

    渲染路由

--- question/question-pusher/render.js
+++ question/question-pusher/render.js
@@ -1,20 +1,77 @@
+/*
+	一些要求
+	 - 可递归
+	 - 100% 测试覆盖率
+*/
+
 
 const is_object = (value) => value !== null && typeof(value) === 'object' && !Array.isArray(value);
 
+class RenderProcessor {
+	renderSingle(struct, vqfQuestion){
+		struct.question.forEach((choice, vqfcCursor) => {
 
-class Render {
-	checkType(vqfStruct, questionStruct) {
-		if (!Array.isArray(vqfStruct)) {
-			throw new Error('vqfStruct 不是一个数组');
-		} else if (!vqfStruct.length) {
-			throw new Error('vqfStruct 不能是空数组');
-		} else if (!Array.isArray(questionStruct)) {
-			throw new Error('vqfQuestion 不是一个数组');
-		} else if (!questionStruct.length) {
-			throw new Error('vqfQuestion 不能是空数组');
-		}
+		});
 	}
 
+	renderMulti(struct){
+
+	}
+
+	renderWhy(struct){
+
+	}
+}
+
+class RenderRouter extends RenderProcessor {
+	fetchRouter(){
+
+	}
+
+	renderRouter(vqfStruct, vqfQuestion, vqfsCursor){
+		if (vqfQuestion === null || typeof(vqfQuestion) !== 'object') {
+			throw new Error('vqfQuestion 不是一个对象或者数组');
+		}
+		const rThis = this;
+		this.typeRouter(vqfStruct, {
+			/* 如果是单选的情况，那么 vqfQuestionStruct 必须是一个对象，并且要有 "choiced" 属性 */
+			single(struct){
+				if (!is_object(vqfQuestion)) {
+					throw new Error(`这个 single vqfQuestion[${vqfsCursor}] 不是一个对象`);
+				} else if (!Number.isInteger(vqfQuestion.choiced) || vqfQuestion.choiced < 0 || vqfQuestion.choiced >= struct.question.length) {
+					throw new Error(`这个 single vqfQuestion[${vqfsCursor}] 的 choiced 属性不合法（不是正整数，或者大于最大选项数目）`);
+				} else {
+					rThis.renderSingle(struct, vqfQuestion);
+				}
+			},
+
+			/** 如果是单选的情况，那么 vqfQuestionStruct 必须是一个数组，
+			*	并且数组元素必须是一个对象，这个对象要有 "choiced" 属性
+			*/
+			multi(struct){
+				if (!Array.isArray(vqfQuestion)) {
+					throw new Error(`这个 multi vqfQuestion[${vqfsCursor}] 不是一个数组`);
+				} else if (vqfQuestion.length > struct.questions.length) {
+					throw new Error(`这个 multi vqfQuestion[${vqfsCursor}] 项目数大于最大选项数`)
+				} else {
+					rThis.renderMulti(struct, vqfQuestion);
+				}
+			},
+
+			/* 如果是单选的情况，那么 vqfQuestionStruct 必须是一个对象，并且要有 "why" 属性 */
+			why(struct){
+				if (!is_object(vqfQuestion)) {
+					throw new Error(`这个 why vqfQuestion[${vqfsCursor}] 不是一个对象`);
+				} else if (typeof(vqfQuestion.why) !== 'string') {
+					throw new Error(`这个 why vqfQuestion[${vqfsCursor}] 的 why 属性不合法（不存在，或者不是字符串）`);
+				} else {
+					rThis.renderWhy(struct, vqfQuestion);
+				}
+			},
+		});
+	}
+
+	/* define项的路由 */
 	typeRouter(struct, callbackObj){
 		if (!is_object(struct)) {
 			throw new Error('struct 不是一个对象');
@@ -30,20 +87,34 @@ class Render {
 
 		if (struct.question !== undefined) {
 			if (Array.isArray(struct.question)) {
-				callbackObj.single();
+				callbackObj.single(struct);
 			} else if (is_object(struct.question) && struct.question.type === 'why') {
-				callbackObj.why();
+				callbackObj.why(struct);
 			} else {
 				throw new Error('struct question 不是一个数组或者对象');
 			}
 		} else if (struct.questions !== undefined) {
 			if (Array.isArray(struct.questions)) {
-				callbackObj.multi();
+				callbackObj.multi(struct);
 			} else {
 				throw new Error('struct questions 不是一个数组')
 			}
 		} else {
 			throw new Error('未知的 struct 类型');
+		}
+	}
+}
+
+class Render extends RenderRouter {
+	checkType(vqfStruct, questionStruct) {
+		if (!Array.isArray(vqfStruct)) {
+			throw new Error('vqfStruct 不是一个数组');
+		} else if (!vqfStruct.length) {
+			throw new Error('vqfStruct 不能是空数组');
+		} else if (!Array.isArray(questionStruct)) {
+			throw new Error('vqfQuestion 不是一个数组');
+		} else if (!questionStruct.length) {
+			throw new Error('vqfQuestion 不能是空数组');
 		}
 	}
 
--- question/question-pusher/test/render.js
+++ question/question-pusher/test/render.js
@@ -39,14 +39,14 @@ describe('render.js 参数检查', () => {
 	});
 });
 
-describe('render.js 渲染', () => {
-	it('类型路由 strcut 不是一个对象', () => {
+describe('render.js TypeRouter', () => {
+	it('strcut 不是一个对象', () => {
 		let render = new Render;
 		(() => {
 			render.typeRouter([], {})
 		}).should.throw('struct 不是一个对象');
 	})
-	it('类型路由 缺少回调句柄', () => {
+	it('缺少回调句柄', () => {
 		let render = new Render;
 		let errorMessage = '回调对象缺少类型句柄';
 		(() => {
@@ -70,7 +70,7 @@ describe('render.js 渲染', () => {
 		});
 	};
 
-	it('类型路由 struct question 不是一个对象或者数组', () => {
+	it('struct question 不是一个对象或者数组', () => {
 		let render = new Render;
 
 		let questionArr = [
@@ -84,7 +84,7 @@ describe('render.js 渲染', () => {
 		}
 	});
 
-	it('类型路由 struct questions 不是一个数组', () => {
+	it('struct questions 不是一个数组', () => {
 		let render = new Render;
 		let questionsArr = [
 			24, 'STRING', true, false, null, {}
@@ -96,7 +96,7 @@ describe('render.js 渲染', () => {
 		}
 	});
 
-	it('类型路由 未知的 struct 类型', () => {
+	it('未知的 struct 类型', () => {
 		let render = new Render;
 		let struct = {};
 
@@ -105,7 +105,7 @@ describe('render.js 渲染', () => {
 		}).should.throw('未知的 struct 类型')
 	});
 
-	it('类型路由', () => {
+	it('正确路由类型', () => {
 		let render = new Render;
 
 		let defineStruct = [
@@ -133,4 +133,108 @@ describe('render.js 渲染', () => {
 		result.should.have.property('multi');
 		result.should.have.property('why');
 	});
+});
+
+describe('render.js RenderRouter', () => {
+	const vqfStruct = {
+
+	};
+	const vqfQuestion = {
+
+	};
+	it('vqfQuestion 不是一个对象或者数组', () => {
+		let render = new Render;
+		let questionArr = [
+			242, null, 'STRING', true, false
+		];
+		for (let question of questionArr) {
+			(() => {
+				render.renderRouter(vqfStruct, question, 0);
+			}).should.throw('vqfQuestion 不是一个对象或者数组');
+		}
+	});
+
+	it('single vqfQuestion 不是一个对象', () => {
+		let render = new Render;
+		let cursor = 0;
+		(() => {
+			render.renderRouter(
+				{ question: [{}] },
+				[],
+				cursor
+			);
+		}).should.throw(`这个 single vqfQuestion[${cursor}] 不是一个对象`);
+	});
+	it('single vqfQuestion choiced 属性不合法', () => {
+		let render = new Render;
+		let choicedArr = [
+			-1, 1, 2, 'a', null, undefined, false, true
+		];
+		for (let choicedKey in choicedArr) {
+			let question = {
+				choiced: choicedArr[choicedKey],
+			};
+			(() => {
+				render.renderRouter(
+					{ question: [{}] },
+					question,
+					choicedKey
+				);
+			}).should.throw(`这个 single vqfQuestion[${choicedKey}] 的 choiced 属性不合法（不是正整数，或者大于最大选项数目）`);
+		}
+	});
+
+	it('multi vqfQuestion 不是一个数组', () => {
+		let render = new Render;
+		let vqfStruct = {questions: [{}]};
+		let questionArr = [
+			{}
+		];
+		for (let questionKey in questionArr) {
+			(() => {
+				render.renderRouter(vqfStruct, questionArr[questionKey], questionKey);
+			}).should.throw(`这个 multi vqfQuestion[${questionKey}] 不是一个数组`);
+		}
+	});
+	it('multi vqfQuestion 项目数大于最大选项数', () => {
+		let render = new Render;
+		let vqfStruct = {questions: [{}, {}]};
+		let questionStruct = [{}, {}, {}];
+		(() => {
+			render.renderRouter(vqfStruct, questionStruct, 0);
+		}).should.throw(`这个 multi vqfQuestion[0] 项目数大于最大选项数`);
+	});
+
+	it('why vqfQuestion 不是一个对象', () => {
+		let render = new Render;
+		let vqfStruct = {question: {type: 'why'}};
+		let questionArr = [
+			[]
+		];
+
+		for (let questionKey in questionArr) {
+			(() => {
+				render.renderRouter(vqfStruct, questionArr[questionKey], questionKey);
+			}).should.throw(`这个 why vqfQuestion[${questionKey}] 不是一个对象`)
+		}
+	});
+
+	it('why vqfQuestion why 属性不存在或者非法', () => {
+		let render = new Render;
+		let vqfStruct = {question: {type: 'why'}};
+		let whyArr = [
+			1, true, false, null, {}, undefined
+		];
+
+		for (let whyKey in whyArr) {
+			(() => {
+				render.renderRouter(vqfStruct, {why: whyArr[whyKey]}, whyKey);
+			}).should.throw(`这个 why vqfQuestion[${whyKey}] 的 why 属性不合法（不存在，或者不是字符串）`)
+		}
+	});
+
+});
+
+describe('render.js Render Processor', () => {
+
 });
