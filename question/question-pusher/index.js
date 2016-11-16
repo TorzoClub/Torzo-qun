79c6daaba2c0c82cd76e3c9b744e2c4b0c66fe7d
vec <vec@moebaka.com>
2016-11-16 15:22:38 +0800

    把 index 作为初始化的模块 剩下的交给 pushchan

--- question/question-pusher/index.js
+++ question/question-pusher/index.js
@@ -1,25 +1,26 @@
 const package = require('./package');
-const config = require('./config');
-const timer = require('./timer');
-const model = require('./model');
-const action = require('./action');
-
-action.sendMessage('噗什酱已经启动', `
-	<p>${config.name} 已经启动了，你现在只需守在时钟前等着晚上十点就行了</p>
-	<p>还有，请不要回复邮件，你的真诚是打动不了程序的</p>
-	<p>作者是 Vec，有 bug 可以到项目主页的 <a href="https://github.com/TorzoClub/Torzo-qun/issues" target="_blank">issue </a> 栏目谈笑风生</p>
-	<footer>version ${package.version}</footer>`,
-	() => {
-		timer.addTask({
-			/* 下午十点 */
-			time: new Date(2016, 11, 22, 00, 00),
-			task(){
-
-			},
-		});
-	}
-)
-
-timer.start(500);
+	config = require('./config'),
+	action = require('./action'),
+	pusherChan = require('./pushchan');
 
 console.info(`Torzo-Question Pusher ${package.version}`);
+
+action.broadcast(config.to,
+	{
+		from: config.mail_opts.auth.user,
+		subject: '噗什酱已经启动',
+		html: `
+			<p>${config.name} 已经启动了，你现在只需守在时钟前等着晚上十点就行了</p>
+			<p>还有，请不要回复邮件，你的真诚是打动不了程序的</p>
+			<p>有 bug 可以到 <a href="https://github.com/TorzoClub/Torzo-qun/issues" target="_blank">项目主页</a> 与作者谈笑风生</p>
+			<footer>version ${package.version}</footer>`,
+	},
+	() => {
+		pusherChan.start();
+	},
+	(err, retry) => {
+		console.warn(err);
+		console.warn('初始化邮件未成功发送');
+		process.exit(-1);
+	}
+);
