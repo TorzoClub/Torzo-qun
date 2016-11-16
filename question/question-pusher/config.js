60d9d8462a292fd0fd641677fcfbfefec3025fd7
vec <vec@moebaka.com>
2016-11-16 19:56:47 +0800

    新增了一些设置项

--- question/question-pusher/config.js
+++ question/question-pusher/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	mail_opts: {
-		host: 'smtp-server.com',
+		host: 'smtp.example.com',
 		port: 465,
 		auth: {
 			user: 'titor@torzo.com',
@@ -9,12 +9,18 @@ module.exports = {
 	},
 	name: '噗什酱',
 
-	api_url: 'http://qun.torzo.com/question/',
+	api_url: 'http://vec.moe/qun/question/',
 	test_api_url: 'http://localhost/qun/question/',
+
+	/* 广播邮件的发送间隔（毫秒） */
+	send_interval: 10000,
+
+	/* 重试间隔（毫秒） */
+	retry_interval: 60000,
 
 	/* 接收邮件的列表 */
 	to: [
-		'receive@torzo.com',
+		'receive@torzo.com'
 	],
 
 	/* 测试用的邮箱地址 */
