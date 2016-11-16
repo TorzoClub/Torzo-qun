0a5c83f4db02cd37768be9ca7a39d681575f73e6
vec <vec@moebaka.com>
2016-11-16 19:01:40 +0800

    link tag blank

--- question/question-pusher/index.js
+++ question/question-pusher/index.js
@@ -12,7 +12,7 @@ action.broadcast(config.to,
 		html: `
 			<p>${config.name} 已经启动了，你现在只需守在时钟前等着晚上十点就行了</p>
 			<p>还有，请不要回复邮件，你的真诚是打动不了程序的</p>
-			<p>有 bug 可以到 <a href="https://github.com/TorzoClub/Torzo-qun/issues" target="_blank">项目主页</a> 与作者谈笑风生</p>
+			<p>有 bug 可以到 <a href="https://github.com/TorzoClub/Torzo-qun/issues" target="_blank"> 项目主页 </a> 与作者谈笑风生</p>
 			<footer>version ${package.version}</footer>`,
 	},
 	() => {
