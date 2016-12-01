5aeaa8c87039a2fed8a3cf66d3551d857aa87e94
vec <vec@moebaka.com>
2016-12-01 10:14:24 +0800

    rename pusherChan to pushChan

--- question/question-pusher/index.js
+++ question/question-pusher/index.js
@@ -2,5 +2,5 @@ Object.assign(exports, {
   package: require('./package'),
   config: require('./config'),
   action: require('./action'),
-  pusherChan: require('./pushchan'),
+  pushChan: require('./pushchan'),
 });
